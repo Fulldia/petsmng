ad73de64d5e234f54ed3ac6185e9943af55f2c28
Fulldia <cam.lafourcade64@gmail.com>
2023-01-23 21:51:29 +0100

    ressources protégées ok

--- app/routes/petmng.routes.js
+++ app/routes/petmng.routes.js
@@ -1,26 +1,27 @@
 module.exports = app => {
   const pets = require("../controllers/petmng.controller.js");
+  const { authJwt } = require("../middleware");
 
   var router = require("express").Router();
 
   //  CREER UN PET
-  router.post("/pets/", pets.create);
+  router.post("/pets/", [authJwt.verifyToken, authJwt.isModeratorOrAdmin], pets.create);
 
   // RECHERCHER UN PET PAR ID
-  router.get("/pets/:pet_id", pets.findOne);
+  router.get("/pets/:pet_id", [authJwt.verifyToken], pets.findOne);
 
 
   //RECUPERER TOUS LES noms de PETS
-  router.get("/pets/", pets.findAll);
+  router.get("/pets/", [authJwt.verifyToken], pets.findAll);
 
   // AFFICHE TOUS LES ANIMAUX QUI NE SONT PAS RESERVES
-  router.get("/notreserved/", pets.findNotReserved);
+  router.get("/notreserved/", [authJwt.verifyToken], pets.findNotReserved);
 
   // MODIFIE UN PET
-  router.put("/pets/:pet_id", pets.update);
+  router.put("/pets/:pet_id", [authJwt.verifyToken, authJwt.isModeratorOrAdmin], pets.update);
 
   // SUPPRIMER UN PET
-  router.delete("/pets/:pet_id", pets.delete);
+  router.delete("/pets/:pet_id", [authJwt.verifyToken, authJwt.isAdmin], pets.delete);
 
 /*
 
